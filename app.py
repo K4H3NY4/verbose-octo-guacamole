bcd2c47e5daee37f01723dd6ef2f88c99670a952
K4H3NY4 <kahenyaa@gmail.com>
2023-07-06 23:21:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,9 @@
-import requests
 import socket
 import json
+import requests
 
 
-# Create a TCP/IP socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to a specific address and port
-server_address = ('localhost', 8000)
-server_socket.bind(server_address)
-
-# Listen for incoming connections
-server_socket.listen(1)
-
-print('Server is running and listening for connections...')
-
-while True:
-    # Wait for a client connection
-    client_socket, client_address = server_socket.accept()
-    print(f'Client connected: {client_address}')
-
+def handle_client(client_socket):
     try:
         # Receive data from the client
         data = client_socket.recv(1024)
@@ -29,20 +13,38 @@ while True:
             print('Received JSON data:')
             url = "http://api.rnd.co.ke/comment"
             payload = json.dumps({"comment": received_data['comment']})
-            headers = { 'Content-Type': 'application/json'}
-            response = requests.request("POST", url, headers=headers, data=payload)
-            response = "Account successfully created"
-            
-            
-              
-            
-
-            # Process the received JSON data
-            # ...
-
+            headers = {'Content-Type': 'application/json'}
+            response = requests.post(url, headers=headers, data=payload)
+            response_text = response.text
+            print('Response:', response_text)
     except Exception as e:
         print(f'Error occurred: {str(e)}')
-
     finally:
         # Close the client socket
         client_socket.close()
+
+
+def run_server():
+    # Create a TCP/IP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to a specific address and port
+    server_address = ('localhost', 8000)
+    server_socket.bind(server_address)
+
+    # Listen for incoming connections
+    server_socket.listen(1)
+
+    print('Server is running and listening for connections...')
+
+    while True:
+        # Wait for a client connection
+        client_socket, client_address = server_socket.accept()
+        print(f'Client connected: {client_address}')
+
+        # Handle the client in a separate thread or process
+        handle_client(client_socket)
+
+
+if __name__ == "__main__":
+    run_server()
